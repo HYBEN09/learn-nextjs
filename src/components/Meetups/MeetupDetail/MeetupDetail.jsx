e80e049c259d0cc6e4df16c6cc49c86349a5c0c1
hyebeen byun <hyebeen2658@naver.com>
2023-04-19 15:07:43 +0900

    feat. 사용자 정의 컴포넌트 및 CSS 모듈 추가

---
+++ src/components/Meetups/MeetupDetail/MeetupDetail.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+import classes from "./MeetupDetail.module.css";
+
+function MeetupDetail(props) {
+  const { image, title, address, description } = props;
+
+  return (
+    <section className={classes.detail}>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <address>{address}</address>
+      <p>{description}</p>
+    </section>
+  );
+}
+
+export default MeetupDetail;
---
+++ src/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/Meetups/MeetupDetail/MeetupDetail";
+
+function MeetupDetailsPage() {
+  return (
+    <MeetupDetail
+      image="https://thespreadissue.files.wordpress.com/2016/04/354a91ebb4654dec1b365e5eff5ddd4b.jpg?w=423&h=282"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetailsPage;
