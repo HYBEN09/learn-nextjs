13c7a47fb5672104b1ca210954a4170b986af4e9
hyebeen byun <hyebeen2658@naver.com>
2023-04-19 11:57:18 +0900

    feat. "_app.js" 파일 및 레이아웃 감싸기

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
+import Layout from "@/components/Layout/Layout";
 import "../styles/globals.css";
 
+// Component는 렌더링 될 실제 페이지 콘텐츠를 저장하고 있는 프로퍼티입니다.
+// pageProps는 페이지가 받는 특수 프로퍼티
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
