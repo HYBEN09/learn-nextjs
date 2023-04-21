9e8003b05e3c1afd3b61e1c28d3c197ad6264a11
hyebeen byun <hyebeen2658@naver.com>
2023-04-21 23:17:42 +0900

    learn."getServerSideProps"를 사용하여 서버 측 렌더링 탐색 #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const DUMMY_MEETUPS = [
     id: "m1",
     title: "A First Meetup",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+      "https://thespreadissue.files.wordpress.com/2016/04/354a91ebb4654dec1b365e5eff5ddd4b.jpg?w=423&h=282",
     address: "Some address 5, 12345 Some City",
     description: "This is a first meetup!",
   },
@@ -23,11 +23,26 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// 주기적인 업데이트로도 부족할 수 있다. ➡️ 요청이 들어올 때마다 페이지를 다시 만들어야 할 때가 있죠.
+// 따라서 페이지를 동적으로 프리 제너레이트해야
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+//* ---------------------------------------------------------------
 export async function getStaticProps() {
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
+    revalidate: 10, // 페이지가 배열 다음에 규칙적으로 업데이트
   };
 }
 
