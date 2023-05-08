1ce1cd0cb6a3416ab3a48b17bbd8d80d187c6efa
hyebeen byun <hyebeen2658@naver.com>
2023-05-08 12:28:49 +0900

    learn. 대체 페이지 사용 및 재배포하기

--- src/pages/new-meetup/index.js
+++ src/pages/new-meetup/index.js
@@ -29,7 +29,7 @@ function NewMeetupPage() {
           content="Add your own meetups and create amazing networking opportunities."
         />
       </Head>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </>
   );
 }
